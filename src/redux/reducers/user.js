d63def25f358e08188066657400dff98ccb37974
shamil isayev <shamil.isayev95@gmail.com>
2023-06-08 18:55:31 +0400

    test

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,18 +1,9 @@
 const initialState = {
-    isUserAuth: false,
-    userData: {},
 };
 
 const user = (state = initialState, action) => {
     const { payload, type } = action;
     switch (type) {
-        case 'CHANGE_USER': {
-            return {
-                ...state,
-                isUserAuth: payload.isUserAuth,
-                userData: payload.userData
-            }
-        }
         default:
             return state;
     }
