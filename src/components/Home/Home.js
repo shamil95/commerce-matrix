bb490f5d47c438d6cb9abf41b9f6f60e00175ee5
shamil isayev <shamil.isayev95@gmail.com>
2023-07-06 20:34:11 +0400

    test

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useTranslation } from "react-i18next";
+import classnames from "classnames";
+import styles from './Home.module.scss'
 
 const Home = () => {
     const { t } = useTranslation();
+    const [showIcon, setShowIcon] = useState(false)
 
     return (
         <div>
-            <h1>{t("home")}</h1>
+            <button onClick={() => setShowIcon(!showIcon)}>clickMe</button>
+            <h1 className={classnames(styles.header, { [styles.active]: !showIcon })}>{t("home")}</h1>
             <div>{t('products')}</div>
         </div>
     );
